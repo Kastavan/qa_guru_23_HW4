e8ed0502b056a3f9e3a7ba4925797022bcbcbdc1
Kastavan <kastavan4eg@gmail.com>
2024-01-20 11:48:11 +0300

    QA_GURU HW4

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-library")
+}
+
+group = "ru.kastavan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("com.codeborne:selenide:6.19.1")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/GithubSelenideSearchTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubSelenideSearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+
+    }
+    @Test
+    void searchTest() {
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+
+}
